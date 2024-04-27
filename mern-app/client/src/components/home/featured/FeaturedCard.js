e3b4e6c35b3369eafbbe24ffd7230a5d83fbc6d5
Felicia Jones <feliciaj628@gmail.com>
2024-04-26 18:49:05 -0700

    change ratings

--- mern-app/client/src/components/home/featured/FeaturedCard.js
+++ mern-app/client/src/components/home/featured/FeaturedCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function FeaturedCard({ delay, title, cuisine }) {
+function FeaturedCard({ delay, title, cuisine, rating }) {
   return (
     <div
       className={`flex flex-col background3 h-[340px] w-[280px] shadow-md hover:shadow-2xl rounded-lg px-5 pt-5 pb-4 drop-in delay-${delay}`}
@@ -12,7 +12,7 @@ function FeaturedCard({ delay, title, cuisine }) {
       <div className="bg-[#dbdbdb] h-[230px] w-[235px] mx-auto mt-2 items-center justify-center flex">
         image
       </div>
-      <div className="mt-2 text-3xl text-yellow-300">★★★★★</div>
+      <div className="mt-2 text-3xl text-yellow-300">{rating}</div>
     </div>
   );
 }
